ea43ba173ba796e906a09ddbb12e70b1e3b23e2d
GilangHap <gilanghappy123@gmail.com>
2025-05-25 22:33:47 +0700

    fix

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<!-- Footer -->
-    <footer class="bg-primary text-white py-6 mt-12">
-        <div class="container mx-auto px-4 text-center">
-            <p>&copy; 2025 londri.in - All Rights Reserved</p>
-        </div>
-    </footer>
